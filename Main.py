d62e8d49f7792f57139c167f9889a2b401d441c8
emfermenty <demrar12345@gmail.com>
2025-07-27 18:40:44 +0500

    update db

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from Services import create_hourly_timeslots
 from start_handler import run_bot
 
 if __name__ == "__main__":
-    asyncio.run(create_hourly_timeslots())
+    #asyncio.run(create_hourly_timeslots())
     print("даты занесены")
     print(datetime.now(timezone.utc) + timedelta(hours=5))
 
